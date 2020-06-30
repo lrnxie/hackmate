ec53a3a2063901a751efc09043fc147a0a983a29
Lauren Xie <lrnxie@gmail.com>
2020-06-30 16:09:11 -0400

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const env = require("dotenv").config({ path: "./config/config.env" });
+const path = require("path");
 const connectDB = require("./config/db");
 
 connectDB();
@@ -12,6 +13,14 @@ app.use("/api/users", require("./routes/users.route"));
 app.use("/api/auth", require("./routes/auth.route"));
 app.use("/api/profile", require("./routes/profile.route"));
 app.use("/api/posts", require("./routes/posts.route"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
